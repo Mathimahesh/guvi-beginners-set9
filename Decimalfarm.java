bcdda01630d42af4938255dd8fb023efc3d05015
MaheshwariS <36835393+Mathimahesh@users.noreply.github.com>
2019-05-08 15:31:40 +0530

    Create Decimalfarm.java

---
+++ Decimalfarm.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double s = a*b;
+        System.out.printf("%.5f",s);
+        
+        
+    }
+}
