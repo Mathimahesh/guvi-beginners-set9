1c19172b6d35e6b66475f05e580e03f9e244d533
MaheshwariS <36835393+Mathimahesh@users.noreply.github.com>
2019-05-08 12:03:02 +0530

    Create Asciivalue.java

---
+++ Asciivalue.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        int s = (int)ch;
+        System.out.println(s);
+    }
+}
